d97748caa0ad86ac57dc1a0c8e7a45343c4c5df2
Sióg Davin Murphy <siogdavinmurphy@gmail.com>
2024-02-01 02:32:06 +0000

    add javascript funtionality to the surprise button

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,6 @@ function filterSelection(c) {
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
-
 // Show filtered elements
 function w3AddClass(element, name) {
   var i, arr1, arr2;
@@ -43,4 +42,25 @@ function w3RemoveClass(element, name) {
       }
     }
     element.className = arr1.join(" ");
-  }
+}
+// Add Functionality to the surprise button
+  function surpriseSelection() {
+    var allFilterFoods = document.getElementsByClassName("filterFoods");
+    
+    // Remove the "show" class from all elements
+    for (var i = 0; i < allFilterFoods.length; i++) {
+        allFilterFoods[i].classList.remove("show");
+    }
+
+    // Get a random index within the range of the elements
+    var randomIndex = Math.floor(Math.random() * allFilterFoods.length);
+
+    // Add the "show" class to the randomly selected element
+    allFilterFoods[randomIndex].classList.add("show");
+}
+
+// Add an event listener to the "Surprise" button
+var surpriseButton = document.querySelector(".moodSurprise .btn-single");
+if (surpriseButton) {
+    surpriseButton.addEventListener("click", surpriseSelection);
+}
